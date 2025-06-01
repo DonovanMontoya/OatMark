75ea6beb7cd9e229918ede38ffee7eab06d077b3
Donovan Montoya <donovan.montoya1@gmail.com>
2025-06-01 18:32:52 -0400

    Add initial project configuration files and integrate MapView component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
