231ffa36d7763d039fe271f7e4e89410052f3b0b
Donovan Montoya <donovan.montoya1@gmail.com>
2025-07-02 20:53:49 -0400

    Update dependencies: @babel/runtime, @babel/traverse, and @babel/types to their latest versions. Fixed env issues

--- services/firebase.js
+++ services/firebase.js
@@ -2,26 +2,27 @@
  * Configure Firebase and export Firestore database instance.
  * Replace placeholders in firebaseConfig with your Firebase project credentials.
  */
+import {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_DATABASE_URL,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_SENDER_ID,
+  VITE_FIREBASE_APP_ID
+} from '@env';
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { FIREBASE_SENDER_ID } from './.env';
-import { FIREBASE_PROJECT_ID } from './.env';
-import { FIREBASE_AUTH_DOMAIN } from './.env';
-import { FIREBASE_STORAGE_BUCKET } from './.env';
-import { FIREBASE_DATABASE_URL } from './.env';
-import { FIREBASE_APP_ID } from './.env';
-import { FIREBASE_API_KEY } from './.env';
 
-// TODO: Replace the following with your Firebase project configuration
-// See: https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_SENDER_ID,
-  appId: FIREBASE_APP_ID,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: VITE_FIREBASE_DATABASE_URL,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
